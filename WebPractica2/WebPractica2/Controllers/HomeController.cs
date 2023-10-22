9a1d2904863ded8a7043abb8e131206d55198c6e
Neptune0001 <93423255+Neptune0001@users.noreply.github.com>
2023-10-22 03:37:45 -0600

    Vistas Agregadas.
    La próxima tarea a realizar es editar las plantillas a usar.

--- WebPractica2/WebPractica2/Controllers/HomeController.cs
+++ WebPractica2/WebPractica2/Controllers/HomeController.cs
@@ -8,23 +8,30 @@ namespace WebPractica2.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //This controller has views
+
+        public ActionResult RegisVendedores()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult RegisVehiculos()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+        public ActionResult ConsulVehiculos()  
+        { 
+            return View(); 
         }
+
     }
 }
